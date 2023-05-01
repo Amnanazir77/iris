35090e4e3f40d1bfd33081c1fd930cc09fec95d1
Amnanazir77 <132253780+Amnanazir77@users.noreply.github.com>
2023-05-01 17:53:33 +0500

    Add files via upload

---
+++ amna (4649).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[5]:
+
+
+pd.read_csv("C:\\Users\\hp\\Downloads\\happyscore_income.csv")
+
+
+# In[6]:
+
+
+happyscore_income=pd.read_csv("C:\\Users\\hp\\Downloads\\happyscore_income.csv")
+
+
+# In[7]:
+
+
+happyscore_income.plot()
+
+
+# In[ ]:
+
+
+
+
